3d064f2bb628db81fc506d8e644fde54ec131b0e
Stéphane P. Péricat <stephane@backbase.com>
2015-03-11 09:49:08 +0100

    fix indent js

--- cxp-fe-training-02/src/main/webapp/static/cxp-fe-training-02/widgets/pf2e1a-feed-reader/js/feed-reader.js
+++ cxp-fe-training-02/src/main/webapp/static/cxp-fe-training-02/widgets/pf2e1a-feed-reader/js/feed-reader.js
@@ -1,20 +1,20 @@
 define([], function() {
-	'use strict';
+    'use strict';
 
-	function FeedReader(widget) {
-		this.widget = widget;
-	}
+    function FeedReader(widget) {
+        this.widget = widget;
+    }
 
-	FeedReader.prototype.init = function() {
-		var self = this;
-		// refresh on preferences changed
-		this.widget.addEventListener('preferencesSaved', function(event) {
-			self.widget.refreshHTML();
-		});
-	};
+    FeedReader.prototype.init = function() {
+        var self = this;
+        // refresh on preferences changed
+        this.widget.addEventListener('preferencesSaved', function(event) {
+            self.widget.refreshHTML();
+        });
+    };
 
-	return function(widget) {
-		var widgetInstance = new FeedReader(widget);
-		widgetInstance.init();
-	};
+    return function(widget) {
+        var widgetInstance = new FeedReader(widget);
+        widgetInstance.init();
+    };
 });
--- cxp-fe-training-02/src/main/webapp/static/cxp-fe-training-02/widgets/pf2e1b-feed-reader/js/feed-reader.js
+++ cxp-fe-training-02/src/main/webapp/static/cxp-fe-training-02/widgets/pf2e1b-feed-reader/js/feed-reader.js
@@ -1,48 +1,48 @@
 define(['portal!jquery'], function($) {
-	'use strict';
+    'use strict';
 
-	var SELECTORS = {
-		MAIN: "[data-js=bt-feed-reader-main]",
-		TEMPLATE: "[data-template=bt-feed-reader-template]"
-	};
+    var SELECTORS = {
+        MAIN: "[data-js=bt-feed-reader-main]",
+        TEMPLATE: "[data-template=bt-feed-reader-template]"
+    };
 
-	var OPTIONS = {
-		PIPE_URL: b$.portal.config.serverRoot + "/proxy?pipe=jsonPipe"
-	};
+    var OPTIONS = {
+        PIPE_URL: b$.portal.config.serverRoot + "/proxy?pipe=jsonPipe"
+    };
 
-	function FeedReader(widget) {
-		this.widget = widget;
-		this.$widgetBody = $(widget.body);
+    function FeedReader(widget) {
+        this.widget = widget;
+        this.$widgetBody = $(widget.body);
 
-		this.$main = this.$widgetBody.find(SELECTORS.MAIN);
+        this.$main = this.$widgetBody.find(SELECTORS.MAIN);
 
-		this.template = this.$widgetBody.find(SELECTORS.TEMPLATE).text();
-	}
+        this.template = this.$widgetBody.find(SELECTORS.TEMPLATE).text();
+    }
 
-	FeedReader.prototype.init = function() {
-		var self = this;
-		this.getJSON();
-		this.widget.addEventListener('preferencesSaved', function(event) {
-			self.getJSON();
-		});
-	};
+    FeedReader.prototype.init = function() {
+        var self = this;
+        this.getJSON();
+        this.widget.addEventListener('preferencesSaved', function(event) {
+            self.getJSON();
+        });
+    };
 
-	FeedReader.prototype.getJSON = function() {
-		var self = this;
-		$.getJSON(OPTIONS.PIPE_URL, {
-			url : self.widget.model.getPreference("url"),
-		}, function(jsonArray) {
-			self.renderItems(jsonArray);
-		});
-	};
+    FeedReader.prototype.getJSON = function() {
+        var self = this;
+        $.getJSON(OPTIONS.PIPE_URL, {
+            url : self.widget.model.getPreference("url"),
+        }, function(jsonArray) {
+            self.renderItems(jsonArray);
+        });
+    };
 
-	FeedReader.prototype.renderItems = function(items) {
-		var html = Mustache.render(this.template, items);
-		this.$main.html(html);
-	};
+    FeedReader.prototype.renderItems = function(items) {
+        var html = Mustache.render(this.template, items);
+        this.$main.html(html);
+    };
 
-	return function(widget) {
-		var widgetInstance = new FeedReader(widget);
-		widgetInstance.init();
-	};
+    return function(widget) {
+        var widgetInstance = new FeedReader(widget);
+        widgetInstance.init();
+    };
 });
