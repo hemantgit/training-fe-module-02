6ceca26b821e57e9d8d04da5583b3aeb42bdb3ec
Stéphane P. Péricat <stephane@backbase.com>
2015-03-23 16:17:27 +0100

    cleanup indentaiton js file

--- cxp-fe-training-02/src/main/webapp/static/cxp-fe-training-02/widgets/pf2e1b-feed-reader/js/pf2e1b-feed-reader.js
+++ cxp-fe-training-02/src/main/webapp/static/cxp-fe-training-02/widgets/pf2e1b-feed-reader/js/pf2e1b-feed-reader.js
@@ -1,83 +1,83 @@
 define(['jquery'], function($) {
-	'use strict';
+    'use strict';
 
-	var self = null,
-		SELECTORS = {
-			'WRAPPER': '[data-js="feed-wrapper"]',
-			'TEMPLATE': '[data-template="articles"]'
-		};
+    var self = null,
+        SELECTORS = {
+            'WRAPPER': '[data-js="feed-wrapper"]',
+            'TEMPLATE': '[data-template="articles"]'
+        };
 
-	/**
-	 * @constructor FeedReader
-	 * @return this
-	 */
-	function FeedReader(widget) {
-		self = this;
+    /**
+    * @constructor FeedReader
+    * @return this
+    */
+    function FeedReader(widget) {
+        self = this;
 
-		self.widget = widget;
-		self.$widget = $(widget.body);
-		self.$wrapper = self.$widget.find(SELECTORS.WRAPPER);
+        self.widget = widget;
+        self.$widget = $(widget.body);
+        self.$wrapper = self.$widget.find(SELECTORS.WRAPPER);
 
-		self.template = self.$widget.find(SELECTORS.TEMPLATE).text();
+        self.template = self.$widget.find(SELECTORS.TEMPLATE).text();
 
-		self.proxyPath = b$.portal.portalServer.serverURL + 'proxy?pipe=jsonPipe';
-		self.feedURL = null;
-		self.getFeedURL();
+        self.proxyPath = b$.portal.portalServer.serverURL + 'proxy?pipe=jsonPipe';
+        self.feedURL = null;
+        self.getFeedURL();
 
-		return self;
-	}
+        return self;
+    }
 
-	/**
-	 * @method init
-	 * @return undefined
-	 */
-	FeedReader.prototype.init = function() {
-		self.getArticles();
+    /**
+    * @method init
+    * @return undefined
+    */
+    FeedReader.prototype.init = function() {
+        self.getArticles();
 
-		// handles preference changes
-		self.widget.addEventListener('preferencesSaved', function() {
-			// get the new url
-			self.getFeedURL();
-			// refresh the articles
-			self.getArticles();
-		});
-	};
+        // handles preference changes
+        self.widget.addEventListener('preferencesSaved', function() {
+            // get the new url
+            self.getFeedURL();
+            // refresh the articles
+            self.getArticles();
+        });
+    };
 
-	/**
-	* @method getFeedURL
-	* @return undefined
-	*/
-	FeedReader.prototype.getFeedURL = function() {
-		self.feedURL = self.widget.model.getPreference('feedURL');
-	};
+    /**
+    * @method getFeedURL
+    * @return undefined
+    */
+    FeedReader.prototype.getFeedURL = function() {
+        self.feedURL = self.widget.model.getPreference('feedURL');
+    };
 
-	/**
-	* @method getArticles
-	* @return undefined
-	*/
-	FeedReader.prototype.getArticles = function() {
-		$.ajax({
-			dataType: 'json',
-			url: self.proxyPath,
-			data: {
-				url: self.feedURL
-			}
-		})
-		.then(function(res) {
-			self.$wrapper.html(self.renderArticles(res));
-		});
-	};
+    /**
+    * @method getArticles
+    * @return undefined
+    */
+    FeedReader.prototype.getArticles = function() {
+        $.ajax({
+            dataType: 'json',
+            url: self.proxyPath,
+            data: {
+                url: self.feedURL
+            }
+        })
+        .then(function(res) {
+            self.$wrapper.html(self.renderArticles(res));
+        });
+    };
 
-	/**
-	 * @method renderArticles
-	 * @return {String} html
-	 */
-	FeedReader.prototype.renderArticles = function(data) {
-		return Mustache.render(self.template, data);
-	};
+    /**
+    * @method renderArticles
+    * @return {String} html
+    */
+    FeedReader.prototype.renderArticles = function(data) {
+        return Mustache.render(self.template, data);
+    };
 
-	return function(widget) {
-		var reader = new FeedReader(widget);
-		reader.init();
-	};
+    return function(widget) {
+        var reader = new FeedReader(widget);
+        reader.init();
+    };
 });
