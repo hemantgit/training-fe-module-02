bdf25f08d70a9cf51306d9b424e9964e92233aea
Stéphane P. Péricat <stephane@backbase.com>
2015-06-25 16:32:10 +0200

    WIP feed readers

---
+++ cxp-fe-training-02/src/main/webapp/static/cxp-fe-training-02/widgets/pf2e1b-feed-reader-widget/scripts/controllers.js
@@ -0,0 +1,32 @@
+/**
+ * Controllers
+ * @module controllers
+ */
+define(function (require, exports) {
+
+    'use strict';
+
+    // @ngInject
+    exports.MainCtrl = function($http, lpWidget) {
+        var ctrl = this; //self this controller
+
+        ctrl.servicePath = b$.portal.portalServer.serverURL + 'services/rest/feed/json';
+
+        ctrl.feedData = null;
+
+        $http({
+            method: 'GET',
+            url: ctrl.servicePath,
+            responseType: 'json',
+            params: {
+                url: lpWidget.model.getPreference('url')
+            }
+        })
+        .success(function(res) {
+            ctrl.feedData = res;
+        })
+        .error(function(e) {
+            throw e;
+        });
+    };
+});
